a0a1589fb7a7ed0ad7e9e182083242979f7e9214
manwild42 <jmw0080@mix.wvu.edu>
2022-02-05 14:27:02 -0500

    Moved files inside app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { bottomNavBarComponent } from 'src/bottomnavbar/bottomnavbar.component';
-import { BuyNowCardsComponent } from 'src/buynowcards/buynowcards.component';
-import { CenterNavBarComponent } from 'src/center-nav-bar/centernavbar.component';
-import { MainCardComponent } from 'src/main-card/maincard.component';
-import { MainSupportCardsComponent } from 'src/mainsupportcards/mainsupportcards.component';
-import { TopNavBarComponent } from 'src/top-nav-bar/topnavbar.component';
+import { bottomNavBarComponent } from 'src/app/bottomnavbar/bottomnavbar.component';
+import { BuyNowCardsComponent } from 'src/app/buynowcards/buynowcards.component';
+import { CenterNavBarComponent } from 'src/app/center-nav-bar/centernavbar.component';
+import { MainCardComponent } from 'src/app/main-card/maincard.component';
+import { MainSupportCardsComponent } from 'src/app/mainsupportcards/mainsupportcards.component';
+import { TopNavBarComponent } from 'src/app/top-nav-bar/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
