aa56bd40d883e5f676238b05f167d9911f66d30b
manwild42 <jmw0080@mix.wvu.edu>
2022-02-07 12:30:23 -0500

    Finsihed all of the buy now cards

--- src/app/buynowcards/mock_buynow_list.ts
+++ src/app/buynowcards/mock_buynow_list.ts
@@ -9,11 +9,29 @@ export const mock_buynow_list = [
     },
 
     {
-        img1:"https://www.games-workshop.com/resources/touts/2022-02-05/GW-Khorne-Lord-Of-Skulls-2022-02-05-HPMulticol-All-bm.jpg",
-        title1: "Khorne Lord of Skulls",
-        img2 : "https://www.games-workshop.com/resources/touts/2022-02-05/GW-Wraithknight-2022-02-05-HPMulticol-All-bm.jpg",
-        title2 : "Wraithknight",
-        img3 : "https://www.games-workshop.com/resources/touts/2021-11-20/GW-Abaddon-The-Despoiler-2021-11-20-New-HPMulticol-All-bm.jpg",
-        title3 : "Abaddon the Despoiler"  
+        img1:"https://www.games-workshop.com/resources/touts/2022-01-29/GW-CadianForce-2022-01-29-NWHPMulticol-All-bm.jpg",
+        title1: "Cadian Defence Force",
+        img2 : "https://www.games-workshop.com/resources/touts/2022-01-01/GW-Mozrog-Skragbad-2022-01-01-New-HPMulticol-All-bm.jpg",
+        title2 : "Morzog Skragbad",
+        img3 : "https://www.games-workshop.com/resources/touts/2022-01-29/GW-Eisenhorn-2022-29-01-ProductHPMulticol-All-bm.jpg",
+        title3 : "Eisenhorn"  
+    },
+
+    {
+        img1:"https://www.games-workshop.com/resources/touts/2022-01-29/GW-Stompa-2022-01-29-NWHPMulticol-All-bm.jpg",
+        title1: "Stompa",
+        img2 : "https://www.games-workshop.com/resources/touts/2022-01-22/GW-Dark%20Apostle%20%20-2022-22-01-ProductHPMulticol-All-bm.jpg",
+        title2 : "Dark Apostle",
+        img3 : "https://www.games-workshop.com/resources/touts/2022-01-22/GW-Teclis-Celennar-2022-01-22-HPMulticol-All-bm.jpg",
+        title3 : "Archemage Teclis and Celennar, Spirit of Hysh"  
+    },
+
+    {
+        img1:"https://www.games-workshop.com/resources/touts/2022-01-29/GW-Karazai-The-Scarred%20-2022-29-01-ProductHPMulticol-All-bm.jpg",
+        title1: "Karazai the Scarred",
+        img2 : "https://www.games-workshop.com/resources/touts/2022-01-22/GW-CawdorRedemptionists-2022-01-22-HPMulticol-All-bm.jpg",
+        title2 : "Cawdor Redemptionists",
+        img3 : "https://www.games-workshop.com/resources/touts/2022-02-05/GW-War-Walker-2022-02-05-HPMulticol-All-bm.jpg",
+        title3 : "War Walker"  
     }
 ];
