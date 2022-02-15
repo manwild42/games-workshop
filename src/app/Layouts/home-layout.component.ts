fd4c7d8740409bf4511b1b6f0c60bca148b03610
manwild42 <jmw0080@mix.wvu.edu>
2022-02-15 16:19:30 -0500

    Fixed cards on home route (forgot to move the constructor)

--- src/app/Layouts/home-layout.component.ts
+++ src/app/Layouts/home-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { BuynowcardItemModel } from "../buynowcards/buynowcard-item.model";
+import { mock_buynow_list } from "../buynowcards/mock_buynow_list";
 
 @Component({
     selector: 'gw-home-layout',
@@ -7,4 +8,12 @@ import { BuynowcardItemModel } from "../buynowcards/buynowcard-item.model";
 })
 export class HomeLayoutComponent{
     buynowcards: BuynowcardItemModel[] = [];
+
+
+    constructor(){
+        for (var buynowcard of mock_buynow_list){
+          console.log(buynowcard);
+          this.buynowcards.push(buynowcard);
+        }
+      }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,5 @@ import { mock_buynow_list } from './buynowcards/mock_buynow_list';
 })
 export class AppComponent {
   title = 'games-workshop';
-  buynowcards: BuynowcardItemModel[] = [];
-
-  constructor(){
-    for (var buynowcard of mock_buynow_list){
-      console.log(buynowcard);
-      this.buynowcards.push(buynowcard);
-    }
-  }
+  
 }
